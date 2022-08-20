e60d3019d3416c8da5b1879890f5278f3696b701
ABHISHEK KUMAR <78287129+akkiibaghel@users.noreply.github.com>
2022-08-20 12:08:58 +0530

    Update email_split.py

--- email_split.py
+++ email_split.py
@@ -1,5 +1,4 @@
 email = input("enter the email_id")
 user_name = email[:email.index("@")]
 domain_name = email[email.index("@") +1:]
-formar_1 =(f"Your name is '{user_name}' and your company name is '{domain_name}' " )
-print(formar_1)
+print(f"Your name is '{user_name}' and your company name is '{domain_name}' " )
